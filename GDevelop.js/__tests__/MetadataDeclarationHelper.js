019c35007a5b38725ea114ef6f3c3967bd25086b
Davy Hélard <davy.helard@gmail.com>
2023-04-23 00:28:43 +0200

    Add tests on free functions.

--- GDevelop.js/__tests__/MetadataDeclarationHelper.js
+++ GDevelop.js/__tests__/MetadataDeclarationHelper.js
@@ -1,6 +1,6 @@
 const initializeGDevelopJs = require('../../Binaries/embuild/GDevelop.js/libGD.js');
 
-describe('MetadataDeclarationHelper.shiftSentenceParamIndexes', () => {
+describe('MetadataDeclarationHelper', () => {
   let gd = null;
   beforeAll((done) =>
     initializeGDevelopJs().then((module) => {
@@ -9,6 +9,141 @@ describe('MetadataDeclarationHelper.shiftSentenceParamIndexes', () => {
     })
   );
 
+  it('can create metadata for free actions', () => {
+    const extension = new gd.PlatformExtension();
+    const project = new gd.Project();
+
+    const eventExtension = project.insertNewEventsFunctionsExtension("MyExtension", 0);
+    const eventsFunction = eventExtension.insertNewEventsFunction("MyFunction", 0);
+    eventsFunction.setFunctionType(gd.EventsFunction.Action);
+    eventsFunction.setFullName("My function");
+
+    const metadataDeclarationHelper = new gd.MetadataDeclarationHelper();
+    metadataDeclarationHelper.generateFreeFunctionMetadata(
+      project,
+      extension,
+      eventExtension,
+      eventsFunction);
+    metadataDeclarationHelper.delete();
+    
+    expect(extension.getAllActions().has("MyFunction")).toBe(true);
+    const action = extension.getAllActions().get("MyFunction");
+    expect(action.getFullName()).toBe("My function");
+
+    extension.delete();
+    project.delete();
+  });
+
+  it('can create metadata for free conditions', () => {
+    const extension = new gd.PlatformExtension();
+    const project = new gd.Project();
+
+    const eventExtension = project.insertNewEventsFunctionsExtension("MyExtension", 0);
+    const eventsFunction = eventExtension.insertNewEventsFunction("MyFunction", 0);
+    eventsFunction.setFunctionType(gd.EventsFunction.Condition);
+    eventsFunction.setFullName("My function");
+
+    const metadataDeclarationHelper = new gd.MetadataDeclarationHelper();
+    metadataDeclarationHelper.generateFreeFunctionMetadata(
+      project,
+      extension,
+      eventExtension,
+      eventsFunction);
+    metadataDeclarationHelper.delete();
+    
+    expect(extension.getAllConditions().has("MyFunction")).toBe(true);
+    const condition = extension.getAllConditions().get("MyFunction");
+    expect(condition.getFullName()).toBe("My function");
+
+    extension.delete();
+    project.delete();
+  });
+
+  it('can create metadata for free expressions', () => {
+    const extension = new gd.PlatformExtension();
+    const project = new gd.Project();
+
+    const eventExtension = project.insertNewEventsFunctionsExtension("MyExtension", 0);
+    const eventsFunction = eventExtension.insertNewEventsFunction("MyFunction", 0);
+    eventsFunction.setFunctionType(gd.EventsFunction.Expression);
+    eventsFunction.setFullName("My function");
+
+    const metadataDeclarationHelper = new gd.MetadataDeclarationHelper();
+    metadataDeclarationHelper.generateFreeFunctionMetadata(
+      project,
+      extension,
+      eventExtension,
+      eventsFunction);
+    metadataDeclarationHelper.delete();
+    
+    expect(extension.getAllExpressions().has("MyFunction")).toBe(true);
+    const expression = extension.getAllExpressions().get("MyFunction");
+    expect(expression.getFullName()).toBe("My function");
+
+    extension.delete();
+    project.delete();
+  });
+
+  it('can create metadata for free ExpressionAndConditions', () => {
+    const extension = new gd.PlatformExtension();
+    const project = new gd.Project();
+
+    const eventExtension = project.insertNewEventsFunctionsExtension("MyExtension", 0);
+    const eventsFunction = eventExtension.insertNewEventsFunction("MyFunction", 0);
+    eventsFunction.setFunctionType(gd.EventsFunction.ExpressionAndCondition);
+    eventsFunction.setFullName("My function");
+
+    const metadataDeclarationHelper = new gd.MetadataDeclarationHelper();
+    metadataDeclarationHelper.generateFreeFunctionMetadata(
+      project,
+      extension,
+      eventExtension,
+      eventsFunction);
+    metadataDeclarationHelper.delete();
+    
+    expect(extension.getAllExpressions().has("MyFunction")).toBe(true);
+    const expression = extension.getAllExpressions().get("MyFunction");
+    expect(expression.getFullName()).toBe("My function");
+    
+    expect(extension.getAllConditions().has("MyFunction")).toBe(true);
+    const condition = extension.getAllConditions().get("MyFunction");
+    expect(condition.getFullName()).toBe("My function");
+
+    extension.delete();
+    project.delete();
+  });
+
+  it('can create metadata for free ActionWithOperator', () => {
+    const extension = new gd.PlatformExtension();
+    const project = new gd.Project();
+
+    const eventExtension = project.insertNewEventsFunctionsExtension("MyExtension", 0);
+
+    const getter = eventExtension.insertNewEventsFunction("Value", 0);
+    getter.setFunctionType(gd.EventsFunction.ExpressionAndConditions);
+    getter.setFullName("Some value");
+
+    const eventsFunction = eventExtension.insertNewEventsFunction("SetValue", 0);
+    eventsFunction.setFunctionType(gd.EventsFunction.ActionWithOperator);
+    eventsFunction.setGetterName("Value");
+
+    const metadataDeclarationHelper = new gd.MetadataDeclarationHelper();
+    metadataDeclarationHelper.generateFreeFunctionMetadata(
+      project,
+      extension,
+      eventExtension,
+      eventsFunction);
+    metadataDeclarationHelper.delete();
+    
+    expect(extension.getAllActions().has("SetValue")).toBe(true);
+    const expression = extension.getAllActions().get("SetValue");
+    expect(expression.getFullName()).toBe("Some value");
+
+    extension.delete();
+    project.delete();
+  });
+
+  describe('shiftSentenceParamIndexes', () => {
   it('give back the sentence when there is no parameters', () => {
     expect(gd.MetadataDeclarationHelper.shiftSentenceParamIndexes('Make an action', 2)).toBe(
       'Make an action'
@@ -26,6 +161,16 @@ describe('MetadataDeclarationHelper.shiftSentenceParamIndexes', () => {
   });
   it('can shift a parameter alone', () => {
     expect(gd.MetadataDeclarationHelper.shiftSentenceParamIndexes('_PARAM2_', 2)).toBe('_PARAM4_');
+  });
+  it(`can shift parameters in a sentence with non-latin characters`, () => {
+    expect(
+      gd.MetadataDeclarationHelper.shiftSentenceParamIndexes(
+        'Скорость превышает _PARAM2_ пикселей в секунду',
+        1
+      )
+    ).toBe(
+      'Скорость превышает _PARAM3_ пикселей в секунду'
+    );
   });
   it("won't shift an ill-formed parameter", () => {
     expect(
@@ -95,4 +240,5 @@ describe('MetadataDeclarationHelper.shiftSentenceParamIndexes', () => {
       );
     });
   });
+});
 });
