d91320107d1edd82d34008793e5d1c8f792f984f
Davy Hélard <davy.helard@gmail.com>
2023-05-26 17:10:59 +0200

    Add a loop parameter.

--- Extensions/3D/Model3DRuntimeObject3DRenderer.ts
+++ Extensions/3D/Model3DRuntimeObject3DRenderer.ts
@@ -242,7 +242,7 @@ namespace gdjs {
       this._action.paused = false;
     }
 
-    playAnimation(animationName: string) {
+    playAnimation(animationName: string, shouldLoop: boolean) {
       this._animationMixer.stopAllAction();
       const clip = THREE.AnimationClip.findByName(
         this._originalModel.animations,
@@ -255,6 +255,8 @@ namespace gdjs {
         return;
       }
       this._action = this._animationMixer.clipAction(clip);
+      this._action.setLoop(shouldLoop ? THREE.LoopRepeat : THREE.LoopOnce, Number.POSITIVE_INFINITY);
+      this._action.clampWhenFinished = true;
       this._action.play();
     }
   }
