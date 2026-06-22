fd65ba4174ca7d601e83143b0e28f2bbbc8dd180
Davy Hélard <davy.helard@gmail.com>
2026-06-22 19:35:01 +0200

    Better display mixed values

--- newIDE/app/src/PropertiesEditor/PropertiesMapToSchema.js
+++ newIDE/app/src/PropertiesEditor/PropertiesMapToSchema.js
@@ -321,8 +321,12 @@ const createField = (
   } else if (valueType === 'objectanimationname') {
     return {
       getChoices: () => {
+        const noAnimationChoice: FieldChoices = {
+          value: '',
+          label: '(no animation)',
+        };
         if (!object) {
-          return [];
+          return [noAnimationChoice];
         }
         // $FlowFixMe[incompatible-type]
         const choices: Array<FieldChoices> = mapFor(
@@ -338,7 +342,7 @@ const createField = (
                 };
           }
         ).filter(Boolean);
-        choices.push({ value: '', label: '(no animation)' });
+        choices.push(noAnimationChoice);
         return choices;
       },
       name,
